236740319116e18e827f9612dff0d86e41f4f78a
Kameron Eves <ccackam@gmail.com>
2020-02-24 14:27:34 -0600

    reorginizes and adds code folding

--- src/skywATCh.cpp
+++ src/skywATCh.cpp
@@ -1,3 +1,4 @@
+// <editor-fold> Legal ---------------------------------------------------------
 /*
 skywATCh ACT program for X-Plane 11.
 
@@ -21,13 +22,16 @@ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 SOFTWARE.
 */
+// </editor-fold> --------------------------------------------------------------
 
-#include "XPLMMenus.h"
-#include "XPWidgets.h"
-#include "XPStandardWidgets.h"
+
+
+// <editor-fold> Headers -------------------------------------------------------
+// Global Libraries
 #include <string>
 #include <stdio.h>
 
+// Operating System specific
 #if IBM
 	#include <windows.h>
 #endif
@@ -39,25 +43,49 @@ SOFTWARE.
 	#include <GL/gl.h>
 #endif
 
+// SDK Libraries
+#include "XPLMMenus.h"
+#include "XPWidgets.h"
+#include "XPStandardWidgets.h"
+
+// SDK Version
 #ifndef XPLM300
 	#error This is made to be compiled against the XPLM300 SDK
 #endif
+// </editor-fold> --------------------------------------------------------------
 
+
+
+// <editor-fold> Gloabl Variables ----------------------------------------------
+// Info
 std::string	 	plugin_name = "skywATCh";
 
+// Menu
 int 					menu_index;
 XPLMMenuID 		menu_container;
 void 					menu_handler(void *, void *);
 
+// Widgets
 static XPWidgetID 			flight_plan_window;
-static XPWidgetID 			pop_widget_id;
+static XPWidgetID 			settings_window;
+// </editor-fold> --------------------------------------------------------------
 
+
+
+// <editor-fold> SDK Required Functions ----------------------------------------
 PLUGIN_API int XPluginStart(char * outName, char * outSig, char *	outDesc)
 {
+	// Plugin info for SDK
 	strcpy(outName, plugin_name.c_str());
 	strcpy(outSig, ("ccackam."+plugin_name).c_str());
 	strcpy(outDesc, "An improved ATC Program.");
 
+	// Create Menu
+	/*
+	- skywATCh
+	  - File Flight Plan
+		- Settings
+	*/
 	menu_index = XPLMAppendMenuItem(XPLMFindPluginsMenu(), "skywATCh", 0, 0);
 	menu_container = XPLMCreateMenu("skywATCh", XPLMFindPluginsMenu(), menu_index, menu_handler, NULL);
 	int flight_plan_index = XPLMAppendMenuItem(menu_container, "File Flight Plan", (void *)"File Flight Plan", 1);
@@ -74,15 +102,27 @@ PLUGIN_API void	XPluginStop(void)
 PLUGIN_API void XPluginDisable(void){}
 PLUGIN_API int  XPluginEnable(void){return 1;}
 PLUGIN_API void XPluginReceiveMessage(XPLMPluginID inFrom, int inMsg, void * inParam){}
+// </editor-fold> --------------------------------------------------------------
 
+
+
+// <editor-fold> Callbacks -----------------------------------------------------
 void menu_handler(void * in_menu_ref, void * in_item_ref)
 {
+	// Determin which menu item was selected.
 	if(!strcmp((const char *)in_item_ref, "File Flight Plan"))
 	{
-		
+
 	}
 	else if(!strcmp((const char *)in_item_ref, "Settings"))
 	{
 
 	}
 }
+// </editor-fold> --------------------------------------------------------------
+
+
+
+// <editor-fold> Internal Functions --------------------------------------------
+
+// </editor-fold> --------------------------------------------------------------
