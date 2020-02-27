3d858c58d04fcb6812c2f4ef0b7d8267bc7347bd
Kameron Eves <ccackam@gmail.com>
2020-02-27 07:16:16 -0600

    changes format and adds background to file flight plan screen

--- Materials/image_converter.py
+++ Materials/image_converter.py
@@ -1,25 +1,22 @@
 import matplotlib.image as mpimg
+import math
 
 img=mpimg.imread('FAA_Flight_Plan_Form.png')
 
 with open("/Users/kameroneves/Library/Application Support/Steam/steamapps/common/X-Plane 11/Resources/plugins/skywATCh/include/FAA_Flight_Plan_Form_cpp_rgb_array.h", "w") as f:
     first_time = True;
-    f.write("float flight_plan_form[699][1107][4] = {")
+    f.write("bool flight_plan_form[699][1107] = {")
     for y in range(len(img)):
-        if not first_time:
-            f.write(",")
-        f.write("{")
         first_time = True;
         for x in range(len(img[0])):
-            if not first_time:
-                f.write(",")
-            f.write("{")
-            first_time = False;
             r = img[y][x][0]
             g = img[y][x][1]
             b = img[y][x][2]
-            a = 1.0
-            f.write('{:.3f},{:.3f},{:.3f},{:.3f}'.format(r,g,b,a))
-            f.write('}')
-        f.write('}')
+            if(math.sqrt(r**2+g**2+b**2)>0.9):
+                if not first_time:
+                    f.write(",")
+                first_time = False;
+                f.write("{")
+                f.write('{},{}',x,y)
+                f.write('}')
     f.write('};')
--- src/skywATCh.cpp
+++ src/skywATCh.cpp
@@ -72,12 +72,8 @@ XPLMMenuID 		menu_container;
 void 					menu_handler(void *, void *);
 
 // Flight Plan Window
-XPLMWindowID 			flight_plan_window_ = NULL;
-void 						read_png();
-void 						create_file_flight_plan_window();
-void						draw_file_flight_plan_window(XPLMWindowID in_window_id, void * in_refcon);
-void 						draw_box(int l,int t,int r,int b);
-void 						get_pixel_color(int x, int y, int width, int height, int& r, int& g, int& b, int& a);
+XPLMWindowID 	flight_plan_window_ = NULL;
+void					draw_file_flight_plan_window(XPLMWindowID in_window_id, void * in_refcon);
 
 // Settings Window
 static XPWidgetID 			settings_window = NULL;
@@ -118,8 +114,6 @@ PLUGIN_API int XPluginStart(char * outName, char * outSig, char *	outDesc)
 	int flight_plan_index = XPLMAppendMenuItem(menu_container, "File Flight Plan", (void *)"File Flight Plan", 1);
 	int settings_index = XPLMAppendMenuItem(menu_container, "Settings", (void *)"Settings", 1);
 
-	create_file_flight_plan_window();
-
 	// Confirm plugin loaded correctly
 	return (flight_plan_index>=0) && (settings_index>=0);
 }
@@ -140,7 +134,37 @@ PLUGIN_API void XPluginReceiveMessage(XPLMPluginID inFrom, int inMsg, void * inP
 // <editor-fold> Callbacks -----------------------------------------------------
 void menu_handler(void * in_menu_ref, void * in_item_ref)
 {
-	// TODO: add menu open window
+	if(!strcmp((const char *)in_item_ref, "File Flight Plan"))
+	{
+		// Get general Parameters
+		int left, top, right, bottom;
+		XPLMGetScreenBoundsGlobal(&left, &top, &right, &bottom);
+
+		// Create Window
+		left += window_inset; top -= window_inset; right -= window_inset; bottom += window_inset;
+		XPLMCreateWindow_t params;
+		params.structSize = sizeof(params);
+		params.left = left;
+		params.top = top;
+		params.right = right;
+		params.bottom = bottom;
+		params.visible = 1;
+		params.drawWindowFunc = draw_file_flight_plan_window;
+		params.handleMouseClickFunc = dummy_mouse_handler;
+		params.handleRightClickFunc = dummy_mouse_handler;
+		params.handleMouseWheelFunc = dummy_wheel_handler;
+		params.handleKeyFunc = dummy_key_handler;
+		params.handleCursorFunc = dummy_cursor_status_handler;
+		params.refcon = NULL;
+		params.layer = xplm_WindowLayerFloatingWindows;
+		params.decorateAsFloatingWindow = 1;
+
+		flight_plan_window_ = XPLMCreateWindowEx(&params);
+		XPLMSetWindowPositioningMode(flight_plan_window_, xplm_WindowPositionFree, -1);
+		XPLMSetWindowGravity(flight_plan_window_, 0, 1, 0, 1); // As the X-Plane window resizes, keep our size constant, and our left and top edges in the same place relative to the window's left/top
+		XPLMSetWindowResizingLimits(flight_plan_window_, 200, 200, 0, 0); // Limit resizing our window: maintain a minimum width/height of 200 boxels and a max width/height of 500
+		XPLMSetWindowTitle(flight_plan_window_, "File Flight Plan");
+	}
 }
 
 void draw_file_flight_plan_window(XPLMWindowID in_window_id, void * in_refcon)
@@ -153,198 +177,41 @@ void draw_file_flight_plan_window(XPLMWindowID in_window_id, void * in_refcon)
 	left += content_inset; top -= content_inset; right -= content_inset; bottom += content_inset;
 	width = right - left; height = top - bottom;
 	left_index = left; top_index = top;
+	float white[] = {1.0,1.0,1.0,1.0};
+	glColor4fv(white);
+	glBegin(GL_QUADS);
+	{
+		glVertex2i(left,top);
+		glVertex2i(right,top);
+		glVertex2i(right,bottom);
+		glVertex2i(left,bottom);
+	}
+	glEnd();
+	float black[] = {0.0,0.0,0.0,1.0};
+	glColor4fv(black);
 	glBegin(GL_POINTS);
 	{
 			for(int i = bottom;i<top;i++)
 			{
 				for(int j = left;j<right;j++)
 				{
-					int r, g, b, x, y;
+					int x, y;
 					x = j-left;
 					y = i-bottom;
 					x = round(std::min(std::max((double)x/(double)width*1107.0,0.0),1107.0));
 					y = 699-round(std::min(std::max((double)y/(double)height*699.0,0.0),699.0));
-					r = flight_plan_form[y][x][0];
-					g = flight_plan_form[y][x][1];
-					b = flight_plan_form[y][x][2];
-					float color[] = {r, g, b, 1.0};
-					glColor4fv(color);
-					glVertex2i(j, i);
+					if(!flight_plan_form[y][x])
+					{
+						glVertex2i(j, i);
+					}
 				}
 			}
 		}
 		glEnd();
-
-
-	// Row 1 -------------------------------------------------------------------
-	// Box A
-	box_width = 0.25*width;
-	box_height = 0.15*height;
-	draw_box(left_index,top_index,left_index+box_width,top_index-box_height);
-	left_index += box_width;
-
-	// Box B
-	box_width = 0.75*width;
-	draw_box(left_index,top_index,left_index+box_width,top_index-box_height);
-	left_index = left;
-	top_index -= box_height;
-
-	// Row 2 -------------------------------------------------------------------
-	// Box 1
-	box_width = 0.1*width;
-	box_height = 0.15*height;
-	draw_box(left_index,top_index,left_index+box_width,top_index-box_height);
-	left_index += box_width;
-
-	// Box 2
-	box_width = 0.15*width;
-	draw_box(left_index,top_index,left_index+box_width,top_index-box_height);
-	left_index += box_width;
-
-	// Box 3
-	box_width = 0.15*width;
-	draw_box(left_index,top_index,left_index+box_width,top_index-box_height);
-	left_index += box_width;
-
-	// Box 4
-	box_width = 0.1*width;
-	draw_box(left_index,top_index,left_index+box_width,top_index-box_height);
-	left_index += box_width;
-
-	// Box 5
-	box_width = 0.2*width;
-	draw_box(left_index,top_index,left_index+box_width,top_index-box_height);
-	left_index += box_width;
-
-	// Box 6
-	box_width = 0.2*width;
-	draw_box(left_index,top_index,left_index+box_width,top_index-box_height);
-	left_index += box_width;
-
-	// Box 7
-	box_width = 0.1*width;
-	draw_box(left_index,top_index,left_index+box_width,top_index-box_height);
-	left_index = left;
-	top_index -= box_height;
-
-	// Row 3 -------------------------------------------------------------------
-	// Box 8
-	box_width = width;
-	box_height = 0.25*height;
-	draw_box(left_index,top_index,left_index+box_width,top_index-box_height);
-	left_index = left;
-	top_index -= box_height;
-
-	// Row 4 -------------------------------------------------------------------
-	// Box 9
-	box_width = 0.25*width;
-	box_height = 0.15*height;
-	draw_box(left_index,top_index,left_index+box_width,top_index-box_height);
-	left_index += box_width;
-
-	// Box 10
-	box_width = 0.15*width;
-	draw_box(left_index,top_index,left_index+box_width,top_index-box_height);
-	left_index += box_width;
-
-	// Box 11
-	box_width = 0.6*width;
-	draw_box(left_index,top_index,left_index+box_width,top_index-box_height);
-	left_index = left;
-	top_index -= box_height;
-
-	// Row 5 -------------------------------------------------------------------
-	// Box 12
-	box_width = 0.15*width;
-	box_height = 0.15*height;
-	draw_box(left_index,top_index,left_index+box_width,top_index-box_height);
-	left_index += box_width;
-
-	// Box 13
-	box_width = 0.25*width;
-	draw_box(left_index,top_index,left_index+box_width,top_index-box_height);
-	left_index += box_width;
-
-	// Box 14
-	box_width = 0.5*width;
-	draw_box(left_index,top_index,left_index+box_width,top_index-box_height);
-	left_index += box_width;
-
-	// Box 15
-	box_width = 0.1*width;
-	draw_box(left_index,top_index,left_index+box_width,top_index-box_height);
-	left_index = left;
-	top_index -= box_height;
-
-	// Row 6 -------------------------------------------------------------------
-	// Box 16
-	box_width = 0.25*width;
-	box_height = 0.15*height;
-	draw_box(left_index,top_index,left_index+box_width,top_index-box_height);
-	left_index += box_width;
-
-	// Box C
-	box_width = 0.75*width;
-	draw_box(left_index,top_index,left_index+box_width,top_index-box_height);
 }
 // </editor-fold> --------------------------------------------------------------
 
 
 
 // <editor-fold> Internal Functions --------------------------------------------
-void create_file_flight_plan_window()
-{
-	// Get general Parameters
-	int left, top, right, bottom;
-	XPLMGetScreenBoundsGlobal(&left, &top, &right, &bottom);
-
-	// Create Window
-	left += window_inset; top -= window_inset; right -= window_inset; bottom += window_inset;
-	XPLMCreateWindow_t params;
-	params.structSize = sizeof(params);
-	params.left = left;
-	params.top = top;
-	params.right = right;
-	params.bottom = bottom;
-	params.visible = 1;
-	params.drawWindowFunc = draw_file_flight_plan_window;
-	params.handleMouseClickFunc = dummy_mouse_handler;
-	params.handleRightClickFunc = dummy_mouse_handler;
-	params.handleMouseWheelFunc = dummy_wheel_handler;
-	params.handleKeyFunc = dummy_key_handler;
-	params.handleCursorFunc = dummy_cursor_status_handler;
-	params.refcon = NULL;
-	params.layer = xplm_WindowLayerFloatingWindows;
-	params.decorateAsFloatingWindow = 1;
-
-	flight_plan_window_ = XPLMCreateWindowEx(&params);
-	XPLMSetWindowPositioningMode(flight_plan_window_, xplm_WindowPositionFree, -1);
-	XPLMSetWindowGravity(flight_plan_window_, 0, 1, 0, 1); // As the X-Plane window resizes, keep our size constant, and our left and top edges in the same place relative to the window's left/top
-	XPLMSetWindowResizingLimits(flight_plan_window_, 200, 200, 0, 0); // Limit resizing our window: maintain a minimum width/height of 200 boxels and a max width/height of 500
-	XPLMSetWindowTitle(flight_plan_window_, "File Flight Plan");
-}
-
-void draw_box(int l,int t,int r,int b)
-{
-	float black[] = {0.0, 0.0, 0.0, 1.0};
-	glColor4fv(black);
-	glBegin(GL_LINE_LOOP);
-	{
-		glVertex2i(l, t);
-		glVertex2i(r, t);
-		glVertex2i(r, b);
-		glVertex2i(l, b);
-	}
-	glEnd();
-}
-
-void get_pixel_color(int x, int y, int width, int height, int& r, int& g, int& b, int& a)
-{
-	x = round(std::min(std::max((double)x/(double)width*1107.0,0.0),1107.0));
-	y = round(std::min(std::max((double)y/(double)height*699.0,0.0),699.0));
-	r = flight_plan_form[y][x][0];
-	g = flight_plan_form[y][x][1];
-	b = flight_plan_form[y][x][2];
-	a = flight_plan_form[y][x][3];
-}
 // </editor-fold> --------------------------------------------------------------
