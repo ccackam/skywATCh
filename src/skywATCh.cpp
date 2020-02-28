f40732be39580d7663522877b68b184ed8d913f3
Kameron Eves <ccackam@gmail.com>
2020-02-28 10:48:52 -0600

    Textures file flight plan screen also adds box drawing for measuring positions in a window.

--- src/skywATCh.cpp
+++ src/skywATCh.cpp
@@ -52,7 +52,11 @@ SOFTWARE.
 #include "XPLMMenus.h"
 #include "XPWidgets.h"
 #include "XPStandardWidgets.h"
-#include "FAA_Flight_Plan_Form_cpp_rgb_array.h"
+#include "texture.h"
+
+#define FOURCC_DXT1 0x31545844 // Equivalent to "DXT1" in ASCII
+#define FOURCC_DXT3 0x33545844 // Equivalent to "DXT3" in ASCII
+#define FOURCC_DXT5 0x35545844 // Equivalent to "DXT5" in ASCII
 
 // SDK Version
 #ifndef XPLM300
@@ -72,23 +76,71 @@ XPLMMenuID 		menu_container;
 void 					menu_handler(void *, void *);
 
 // Flight Plan Window
-XPLMWindowID 	flight_plan_window_ = NULL;
-void					draw_file_flight_plan_window(XPLMWindowID in_window_id, void * in_refcon);
+XPLMWindowID 				flight_plan_window_ = NULL;
+void								draw_file_flight_plan_window(XPLMWindowID in_window_id, void * in_refcon);
+PPL::Texture* 			flight_plan_texture;
 
 // Settings Window
 static XPWidgetID 			settings_window = NULL;
 
 // Settings
 
-// Flight Plan Info
+// Internal Settings
 int window_inset = 100;
 int content_inset = 50;
+
+// Flight Plan Info
 
 // General Functions
 int					dummy_mouse_handler(XPLMWindowID in_window_id, int x, int y, int is_down, void * in_refcon) { return 0; }
 XPLMCursorStatus	dummy_cursor_status_handler(XPLMWindowID in_window_id, int x, int y, void * in_refcon) { return xplm_CursorDefault; }
 int					dummy_wheel_handler(XPLMWindowID in_window_id, int x, int y, int wheel, int clicks, void * in_refcon) { return 0; }
 void				dummy_key_handler(XPLMWindowID in_window_id, char key, XPLMKeyFlags flags, char virtual_key, void * in_refcon, int losing_focus) { }
+
+// Utilities
+bool draw_bounding_box = 0;
+float indexes[4] = {0,0,0,0};
+XPLMCursorStatus	courser_box_maker(XPLMWindowID in_window_id, int x, int y, void * in_refcon)
+{
+	int left, top, right, bottom, width, height;
+	XPLMGetWindowGeometry(in_window_id,&left, &top, &right, &bottom);
+	left += content_inset;
+	top -= content_inset;
+	right -= content_inset;
+	bottom += content_inset;
+	width = right-left;
+	height = top-bottom;
+	if(draw_bounding_box)
+	{
+		indexes[2]=(float)(x-left)/(float)width;
+		indexes[3]=(float)(y-bottom)/(float)height;
+	}
+	return xplm_CursorDefault;
+}
+int					mouse_box_maker(XPLMWindowID in_window_id, int x, int y, int is_down, void * in_refcon)
+{
+	if(is_down=xplm_MouseDown)
+	{
+		int left, top, right, bottom, width, height;
+		XPLMGetWindowGeometry(in_window_id,&left, &top, &right, &bottom);
+		left += content_inset;
+		top -= content_inset;
+		right -= content_inset;
+		bottom += content_inset;
+		width = right-left;
+		height = top-bottom;
+		indexes[0+draw_bounding_box*2]=(float)(x-left)/(float)width;
+		indexes[1+draw_bounding_box*2]=(float)(y-bottom)/(float)height;
+		draw_bounding_box = !draw_bounding_box;
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+
+int loadDDS(const char * imagepath);
 
 
 // </editor-fold> --------------------------------------------------------------
@@ -114,6 +166,8 @@ PLUGIN_API int XPluginStart(char * outName, char * outSig, char *	outDesc)
 	int flight_plan_index = XPLMAppendMenuItem(menu_container, "File Flight Plan", (void *)"File Flight Plan", 1);
 	int settings_index = XPLMAppendMenuItem(menu_container, "Settings", (void *)"Settings", 1);
 
+	flight_plan_texture = new PPL::Texture("/Users/kameroneves/Library/Application Support/Steam/steamapps/common/X-Plane 11/Resources/plugins/skywATCh/Materials/FAA_Flight_Plan_Form.bmp",1);
+
 	// Confirm plugin loaded correctly
 	return (flight_plan_index>=0) && (settings_index>=0);
 }
@@ -122,6 +176,7 @@ PLUGIN_API void	XPluginStop(void)
 {
 	XPLMDestroyMenu(menu_container);
 	XPLMDestroyWindow(flight_plan_window_);
+	flight_plan_texture->~Texture();
 }
 
 PLUGIN_API void XPluginDisable(void){}
@@ -150,11 +205,11 @@ void menu_handler(void * in_menu_ref, void * in_item_ref)
 		params.bottom = bottom;
 		params.visible = 1;
 		params.drawWindowFunc = draw_file_flight_plan_window;
-		params.handleMouseClickFunc = dummy_mouse_handler;
+		params.handleMouseClickFunc = mouse_box_maker;
 		params.handleRightClickFunc = dummy_mouse_handler;
 		params.handleMouseWheelFunc = dummy_wheel_handler;
 		params.handleKeyFunc = dummy_key_handler;
-		params.handleCursorFunc = dummy_cursor_status_handler;
+		params.handleCursorFunc = courser_box_maker;
 		params.refcon = NULL;
 		params.layer = xplm_WindowLayerFloatingWindows;
 		params.decorateAsFloatingWindow = 1;
@@ -169,45 +224,46 @@ void menu_handler(void * in_menu_ref, void * in_item_ref)
 
 void draw_file_flight_plan_window(XPLMWindowID in_window_id, void * in_refcon)
 {
-	XPLMSetGraphicsState(0,0,0,0,1,1,0);
-
-	// Content Box
-	int left, top, right, bottom, width, height, left_index, top_index, char_height, box_height, box_width;
+	int left, top, right, bottom, l, t, r, b, width, height;
 	XPLMGetWindowGeometry(in_window_id, &left, &top, &right, &bottom);
-	left += content_inset; top -= content_inset; right -= content_inset; bottom += content_inset;
-	width = right - left; height = top - bottom;
-	left_index = left; top_index = top;
-	float white[] = {1.0,1.0,1.0,1.0};
-	glColor4fv(white);
-	glBegin(GL_QUADS);
+
+	left += content_inset;
+	top -= content_inset;
+	right -= content_inset;
+	bottom += content_inset;
+	width = right-left;
+	height = top-bottom;
+
+	XPLMSetGraphicsState(0,1,0,0,1,0,0);
+	glBlendFunc(GL_SRC_ALPHA,GL_ONE_MINUS_SRC_ALPHA);
+	glColor4f(1,1,1,1);
+
+	XPLMBindTexture2d(flight_plan_texture->id(), 0);
+  glBegin(GL_QUADS);
 	{
-		glVertex2i(left,top);
-		glVertex2i(right,top);
-		glVertex2i(right,bottom);
-		glVertex2i(left,bottom);
+	  glTexCoord2f(0,1); glVertex2f(left, top);
+	  glTexCoord2f(1,1); glVertex2f(right, top);
+	  glTexCoord2f(1,0); glVertex2f(right, bottom);
+	  glTexCoord2f(0,0); glVertex2f(left, bottom);
 	}
+  glEnd();
+	l = round(fmin(indexes[0],indexes[2])*width+left);
+	r = round(fmax(indexes[0],indexes[2])*width+left);
+	b = round(fmin(indexes[1],indexes[3])*height+bottom);
+	t = round(fmax(indexes[1],indexes[3])*height+bottom);
+	float green[4] = {0.0,1.0,0.0,1.0};
+	glColor4fv(green);
+	glBegin(GL_LINE_LOOP);
+		 glVertex2f(l, b);
+		 glVertex2f(r, b);
+		 glVertex2f(r, t);
+		 glVertex2f(l, t);
 	glEnd();
-	float black[] = {0.0,0.0,0.0,1.0};
-	glColor4fv(black);
-	glBegin(GL_POINTS);
-	{
-			for(int i = bottom;i<top;i++)
-			{
-				for(int j = left;j<right;j++)
-				{
-					int x, y;
-					x = j-left;
-					y = i-bottom;
-					x = round(std::min(std::max((double)x/(double)width*1107.0,0.0),1107.0));
-					y = 699-round(std::min(std::max((double)y/(double)height*699.0,0.0),699.0));
-					if(!flight_plan_form[y][x])
-					{
-						glVertex2i(j, i);
-					}
-				}
-			}
-		}
-		glEnd();
+
+	char scratch_buffer[150];
+	float col_white[] = {1.0, 1.0, 1.0};
+	sprintf(scratch_buffer, "Coordinates ( %f0.5 , %f0.5 ) ( %f0.5 , %f0.5 )", indexes[0], indexes[1], indexes[2], indexes[3]);
+	XPLMDrawString(col_white, left, bottom-10, scratch_buffer, NULL, xplmFont_Proportional);
 }
 // </editor-fold> --------------------------------------------------------------
 
